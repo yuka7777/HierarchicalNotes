f1c859aa383d578e14eb0238ab18ea28fb4bba47
yuka7777 <yuka37270@gmail.com>
2024-08-11 21:50:31 +0900

    Update NoteStyle.js

---
+++ styles/NoteStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  noteContainer: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 5,
+  },
+  noteText: {
+    fontSize: 16,
+    color: '#333',
+  },
+});
