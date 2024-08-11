7ad8f3a9e135885db773bfe64d6d831a7071a4dd
yuka7777 <yuka37270@gmail.com>
2024-08-11 22:51:43 +0900

    Delete models directory

--- models/Note.js
+++
@@ -1,9 +0,0 @@
-export default class Note {
-  constructor(id, content, createdAt, updatedAt, folderId = null) {
-    this.id = id; // メモの一意のID
-    this.content = content; // メモの内容
-    this.createdAt = createdAt; // 作成日時
-    this.updatedAt = updatedAt; // 更新日時
-    this.folderId = folderId; // メモが所属するフォルダーのID（オプション）
-  }
-}
