d43f646e8ca684cee1ca8f43e5597a6697206f11
yuka7777 <yuka37270@gmail.com>
2024-08-11 21:50:51 +0900

    Update FolderStyle.js

---
+++ styles/FolderStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  folderContainer: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 5,
+  },
+  folderText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
