67b1a6d9ca61adcfbe2158a99f5aa5c6ac6f63d1
yuka7777 <yuka37270@gmail.com>
2024-08-12 18:20:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 // ルートへのリクエストに対するレスポンス
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/home.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
